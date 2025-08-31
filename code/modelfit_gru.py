bab8707aaa18bee9763a76149285e8962feeeda1
Hayoung Song <48370674+hyssong@users.noreply.github.com>
2025-08-31 15:14:22 -0500

    Update modelfit_gru.py

--- code/modelfit_gru.py
+++ code/modelfit_gru.py
@@ -7,7 +7,7 @@ import torch
 import sys
 import pickle
 
-directory = '/Users/hayoungsong/Documents/_postdoc/modelmind/github'
+directory = '/directory' # ****** set current directory ******
 sys.path.append(directory+'/model')
 from gru import gru
 
